daccd7a68ed8db0c69f634a154f43dacf41419ba
Souptikbej <bejsouptik@gmail.com>
2025-11-09 23:57:53 +0530

    day8

--- Asynco.js
+++ Asynco.js
@@ -1,16 +1,27 @@
-function fetchData() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("User data fetched!");
-        }, 2000);
-    });
+// function fetchData() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("User data fetched!");
+//         }, 2000);
+//     });
+// }
+
+// async function showData() {
+//     console.log("Fetching...");
+//     const result = await fetchData();
+//     console.log(result);
+//     console.log("Done!");
+// }
+
+// showData();
+async function getUsers() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        console.log(data);
+    } catch (err) {
+        console.log("Error:", err);
+    }
 }
 
-async function showData() {
-    console.log("Fetching...");
-    const result = await fetchData(); // waits here
-    console.log(result);
-    console.log("Done!");
-}
-
-showData();
+getUsers();
