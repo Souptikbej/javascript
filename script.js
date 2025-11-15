74900faac4f69ce765ca831997a9788f3bc524c2
Souptikbej <bejsouptik@gmail.com>
2025-11-15 22:02:06 +0530

    day13

---
+++ script.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+// fs.writeFile("Hello.txt", "My name is Souptik bej", function (err) {
+//     if (err) console.log(err)
+//     else console.log("File Create Successfully.....")
+// })
+
+// fs.appendFile("Hello.txt", "I am a Good Coder", function (err) {
+//     if (err) console.log(err)
+//     else console.log("Append Successfully.....")
+// })
+
+// fs.rename("Hello.txt", "Hey.txt", function (err) {
+//     if (err) console.log(err)
+//     else console.log("Rename Succesfully......")
+// })
